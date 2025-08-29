8bc02a9cb4d41eedefb3473c03a23f80adf68a54
tetukocatons <tetukocs@hotmail.com>
2025-08-29 07:57:50 +0000

    Refactor: Hapus file redundan dan perbaiki struktur kode

--- src/lib/supabaseServer.ts
+++
@@ -1,6 +0,0 @@
-import { cookies } from 'next/headers';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-
-export const serverClient = () => createServerComponentClient({ cookies });
-export const routeClient  = () => createRouteHandlerClient({ cookies });
